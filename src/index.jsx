2fef7b159a96b901ddf192193b8fcac03f612179
Umair Riaz <umairriaz@Umairs-MacBook-Pro-2.local>
2021-12-19 02:03:51 +0500

    prettier and eslint added

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
+// Packaged Imports here!!
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+
+// Non-packaged Imports here!!
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from 'redux/store';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
